1b443677b0e9029277d46cee64e52c12bfe8436c
Jesse Doyle <jdoyle@ualberta.ca>
2021-06-03 20:48:47 -0600

    chore(ci): Move to GitHub Actions
    * Migrate away from TravisCI to GitHub Actions as
      it's easier to setup, more common, faster and generally
      maintained better.

--- src/duktape/version.cr
+++ src/duktape/version.cr
@@ -14,9 +14,9 @@ module Duktape
   end
 
   module VERSION
-    MAJOR =  1
+    MAJOR = 1
     MINOR = 0
-    TINY  =  0
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join "."
