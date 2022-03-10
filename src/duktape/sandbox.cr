2de9df1ca2c4cca04c27117bb2acdbf378207946
Zac Nowicki <zachnowicki@gmail.com>
2022-03-09 19:42:59 -0500

    Replace Sandbox @timeout with @timeout_data for udata
    The goal is to alter the behavior of Sandbox so that the execution
    timeout is measured from the time of calling #eval, instead of the time
    of creating the sandbox.
    
    To achieve this, instead of placing a timeout struct on the heap and
    passing that as the user data pointer, we allocate a mutable Crystal
    class with a start point field that we can update prior to engaging the
    VM.
    
    I also had to do two monkeypatches to the Context methods that serve as
    the edge of Crystal code before calling into LibDuk. This may not be the
    proper way to do this, but it is an awkward situation, and Sandbox is
    the only descendant of Context, so I'm not sure what the intention is..
    
    The next commit will complete the wiring of this new object.
    
    ---
    
    As a bonus, this adds a ctor that takes a time span instead of just an
    int, so you can expressively decalre the timeout as such:
    
        Duktape::Sandbox.new(500.milliseoncds)
    
    instead of having to check what units are expected.

--- src/duktape/sandbox.cr
+++ src/duktape/sandbox.cr
@@ -5,14 +5,27 @@
 # This is free software. Please see LICENSE for details.
 
 module Duktape
+  class TimeoutData
+    property start : Time::Span
+    property timeout : Time::Span
+
+    def initialize(@start, @timeout)
+    end
+
+    def elapsed? : Bool
+      dt = Time.monotonic - @start
+      dt > timeout
+    end
+  end
+
   class Sandbox < Context
     include Support::Time
-    getter timeout : Int32 | Int64 | Nil
+
+    @timeout_data : TimeoutData?
 
     def initialize
       @ctx = Duktape.create_heap_default
       @heap_destroyed = false
-      @timeout = nil
       @should_gc = true
       builtin_functions
       secure!
@@ -21,7 +34,6 @@ module Duktape
     def initialize(context : LibDUK::Context)
       @ctx = context
       @heap_destroyed = false
-      @timeout = nil
       # NOTE: Don't automatically destroy the heap
       # on finalization when given a `LibDUK::Context`.
       @should_gc = false
@@ -29,16 +41,20 @@ module Duktape
       secure!
     end
 
-    def initialize(timeout : Int32 | Int64)
-      timeout = timeout.to_i64
-      if timeout < 100
+    def self.new(timeout : Int32 | Int64)
+      # NOTE(z64): backwards compatibility
+      Sandbox.new(timeout.milliseconds)
+    end
+
+    def initialize(timeout : Time::Span)
+      if timeout < 100.milliseconds
         raise ArgumentError.new "timeout must be > 100ms"
       else
-        udata = make_udata timeout
-        @ctx = Duktape.create_heap_udata udata
+        timeout_data = TimeoutData.new(Time::Span.zero, timeout)
+        @ctx = Duktape.create_heap_udata(timeout_data.unsafe_as(Pointer(Void)))
+        @timeout_data = timeout_data
       end
       @heap_destroyed = false
-      @timeout = timeout
       @should_gc = true
       builtin_functions
       secure!
@@ -48,16 +64,15 @@ module Duktape
       true
     end
 
-    def timeout?
-      timeout != nil
+    def timeout : Int32 | Int64 | Nil
+      # NOTE(z64): backwards compatibility
+      @timeout_data.try do |td|
+        td.timeout.total_milliseconds.to_i64
+      end
     end
 
-    private def make_udata(timeout : Int64)
-      start = current_time_nano
-      tv = timeout_timeval timeout
-      data = LibDUK::TimeoutData.new start: start, timeout: tv
-      slc = Slice(LibDUK::TimeoutData).new 1, data
-      slc.to_unsafe.as(Void*)
+    def timeout?
+      !@timeout_data.nil?
     end
 
     private def secure!
@@ -78,6 +93,39 @@ module Duktape
       push_string "Duktape"
       del_prop -2
       pop
+    end
+
+    # NOTE(z64): we monkey patch the following two eval_string methods from
+    # Context. this is so we can reset the start time within @timeout_data,
+    # which our timeout callback will read in duk_cr_timeout.
+
+    def eval_string(src : String)
+      flags = LibDUK::Compile.new(0_u32) |
+              LibDUK::Compile::Eval |
+              LibDUK::Compile::NoSource |
+              LibDUK::Compile::StrLen |
+              LibDUK::Compile::Safe |
+              LibDUK::Compile::NoFilename
+
+      if td = @timeout_data
+        td.start = Time.monotonic
+      end
+      LibDUK.eval_raw ctx, src, 0, flags
+    end
+
+    def eval_string_noresult(src : String)
+      flags = LibDUK::Compile.new(0_u32) |
+              LibDUK::Compile::Eval |
+              LibDUK::Compile::Safe |
+              LibDUK::Compile::NoSource |
+              LibDUK::Compile::StrLen |
+              LibDUK::Compile::NoResult |
+              LibDUK::Compile::NoFilename
+
+      if td = @timeout_data
+        td.start = Time.monotonic
+      end
+      LibDUK.eval_raw ctx, src, 0, flags
     end
   end
 end
